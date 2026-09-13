7f965ba5c53c5a4621ba6b5f45b8c03ba6af8b92
Aashi-21245 <aashimishra2109@gmail.com>
2026-09-13 18:54:37 +0530

    Update backend URL to Render production URL

--- career-guidance-frontend/src/api/axios.js
+++ career-guidance-frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://career-guidance-backend-upth.onrender.com/api',
 });
 
 API.interceptors.request.use((config) => {
